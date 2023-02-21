447a3bde4f60f25a8874fc3ab86a51bbda9f7ed8
Amir Ashlag <ashlag.amir@gmail.com>
2023-02-21 23:36:04 +0200

    O

---
+++ src/components/BetModal/BetModal.jsx
@@ -0,0 +1,95 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "./BetModal.css";
+import { useContext } from "react";
+import { UserContext2 } from "../../Context/UserContext/UserContext";
+
+function BetModal({ overUnder, ratio, team, amount, setShowModal, id }) {
+  const [error, seterror] = useState(false);
+  const { currentUser } = useContext(UserContext2);
+  const [uploaded, setUploaded] = useState(false)
+
+  useEffect(() => {
+    console.log(currentUser)
+    if (!team || !amount) {
+      seterror(true);
+    }
+  }, []);
+
+  function clickHandler(){
+    const bet = {
+      type: "game score",
+      gameId: id,
+      amount: amount,
+      userOne: {
+        id: currentUser._id,
+        bet: {
+          winner: team,
+          overUnder: overUnder
+        },
+      },
+    };
+    axios.post("http://localhost:8080/bets", bet).then((res)=>{
+        console.log(res)
+        setUploaded(true)
+    })
+  }
+
+  return (
+    <div
+      className="modal show modal-position"
+      style={{ display: "block", position: "initial" }}
+    >
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>Your bet</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {error && <div>please enter team and amount</div>}
+          {!error && overUnder < 0 && (
+            <div>
+              If the {team} win or loses by less then {-overUnder} you will win{" "}
+              {amount} and your opponent will lose {amount * ratio}
+            </div>
+          )}
+          {!error && overUnder > 0 && (
+            <div>
+              If the {team} win by more then {overUnder} you will win {amount}{" "}
+              and your opponent will lose {amount * ratio}
+            </div>
+          )}
+          {!error && overUnder === 0 && (
+            <div>
+              If the {team} win you will win {amount} and your opponent will
+              lose {amount * ratio}
+            </div>
+          )}
+          {uploaded && <h5 className="bet-uploaded">Bet uploaded!</h5>}
+        </Modal.Body>
+
+        <Modal.Footer className="footer">
+          {!uploaded && (
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
+              Cancel
+            </Button>
+          )}
+          {!uploaded && (
+            <Button variant="primary" onClick={clickHandler}>
+              Upload
+            </Button>
+          )}
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  );
+}
+
+export default BetModal;
--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -6,16 +6,21 @@ import "./GameCard.css";
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import { UserContext2 } from "../../Context/UserContext/UserContext";
+import BetModal from "../BetModal/BetModal";
+import { Ratio } from "react-bootstrap";
 
 function GameCard({ gameId }) {
   const [gameData, setGameData] = useState("");
   const [bet, setbBet] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [team, setTeam] = useState(false);
+  const [amount, setAmount] = useState(false);
+  const [overUnder, setOverUnder] = useState(false)
+  const [ratio, setRatio] = useState(false);
 
   const location = useLocation();
   const pathname = location.pathname;
   const id = pathname.split("/")[2];
-  const { currentUser } = useContext(UserContext2);
-  // console.log(currentUser)
 
   useEffect(() => {
     axios.get(`http://localhost:8080/games/ById/${id}`).then((res) => {
@@ -92,59 +97,105 @@ function GameCard({ gameId }) {
             </div>
           )}
         </Modal.Footer>
-        {bet && (
+        {!showModal && bet && (
           <motion.div layout className={bet ? "bet" : "bet-none"}>
             <motion.div className="bet-title">New bet</motion.div>
             <motion.div className="bet-container">
               <form>
                 <div className="row">
                   <div className="col">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="select team"
-                    />
+                    <span className="description">select your team</span>
+                    <select
+                      className="select-team"
+                      value={team}
+                      onChange={(e) => {
+                        setTeam(e.target.value);
+                      }}
+                    >
+                      <option value={false}>select team</option>
+                      <option value={gameData.teams.home.name}>
+                        {gameData.teams.home.nickname}
+                      </option>
+                      <option value={gameData.teams.visitors.name}>
+                        {gameData.teams.visitors.nickname}
+                      </option>
+                    </select>
                   </div>
                   <div className="col">
+                    <span className="description">Over under (optional)</span>
                     <input
-                      type="text"
+                      type="number"
                       className="form-control"
-                      placeholder="select plus minus"
+                      placeholder="select over under"
+                      min={-50}
+                      max={50}
+                      step={1}
+                      value={overUnder}
+                      onChange={(e) => {
+                        setOverUnder(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
                 <div className="top">
                   <div className="row">
                     <div className="col">
+                      <span className="description">select bet amount</span>
                       <input
                         type="number"
                         className="form-control"
-                        placeholder="select your bet amount"
+                        placeholder="amount"
                         min={10}
                         max={1000}
                         step={5}
+                        value={amount}
+                        onChange={(e) => {
+                          setAmount(e.target.value);
+                        }}
                       />
                     </div>
                     <div className="col">
+                      <span className="description">bet ratio (optional)</span>
                       <input
                         min={0.1}
                         max={10}
                         step={0.1}
                         type="number"
                         className="form-control"
-                        placeholder="bet ratio for oponnent"
+                        placeholder="bet ratio"
+                        value={ratio}
+                        onChange={(e) => {
+                          setRatio(e.target.value);
+                        }}
                       />
                     </div>
                   </div>
                 </div>
               </form>
             </motion.div>
-            <Button variant="primary" className="submit-bet">
+            <Button
+              variant="primary"
+              className="submit-bet"
+              onClick={() => {
+                setShowModal(true);
+              }}
+            >
               Upload bet
             </Button>
           </motion.div>
         )}
       </Modal.Dialog>
+      {showModal && (
+        <BetModal
+          className="bet-modal"
+          ratio={ratio}
+          amount={amount}
+          team={team}
+          overUnder={overUnder}
+          setShowModal={setShowModal}
+          id = {gameData.id}
+        />
+      )}
     </div>
   );
 }
