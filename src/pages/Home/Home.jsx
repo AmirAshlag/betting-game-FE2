993f14925e2831201e8806ca245ee3fa80210037
Mofaz Shmuely <mofazshmuely69@gmail.com>
2023-02-23 13:44:04 +0200

    o

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import GameList from "../../components/GameList/GameList";
 import "./Home.css";
@@ -7,24 +7,22 @@ import "react-calendar/dist/Calendar.css";
 import axios from "axios";
 
 export default function Home() {
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(false);
   const [teams, setTeams] = useState([]);
   const [games, setGames] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/games/teams")
-      .then((res) => res.json())
-      .then((data) => {
-        setTeams(data);
-      });
-  }, []);
-
   // useEffect(() => {
-  //   axios.get(`http://localhost:8080/games/ByDate/${date}`).then((res) => {
-  //     console.log(res.data.response);
-  //     setGames(res.data.response);
+  //   axios.get("http://localhost:8080/games/teams").then((res) => {
+  //     console.log(res.data);
   //   });
-  // }, [date]);
+  // }, []);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/games/ByDate/${date}`).then((res) => {
+      console.log(res.data.response);
+      setGames(res.data.response);
+    });
+  }, [date]);
 
   const getGamesPerTeam = (teamId) => {
     fetch(`http://localhost:8080/games/teams/${teamId}`)
@@ -76,7 +74,7 @@ export default function Home() {
             </div>
 
             <Dropdown.Menu>
-              {teams.map((team) => {
+              {/* {teams.map((team) => {
                 return (
                   <Dropdown.Item
                     key={team.id}
@@ -85,7 +83,7 @@ export default function Home() {
                     {team.name || team.nickname}
                   </Dropdown.Item>
                 );
-              })}
+              })} */}
             </Dropdown.Menu>
           </div>
         </Dropdown>
